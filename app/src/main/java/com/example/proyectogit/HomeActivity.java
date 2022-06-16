3532d22b29d21b6e1111947227722bcde0c52e13
EderInk <jossimar.leon.19@gmail.com>
2022-06-16 01:43:49 -0500

    Se pasan datos del intent

--- app/src/main/java/com/example/proyectogit/HomeActivity.java
+++ app/src/main/java/com/example/proyectogit/HomeActivity.java
@@ -3,12 +3,19 @@ package com.example.proyectogit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity {
-
+    TextView usuario;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Bundle extras = getIntent().getExtras();
+        String emailUsuario = extras.getString("email"); // Se pasó el nombre de la variable del intent
+        String nombreUsuario = extras.getString("nombre"); // Se pasó el nombre
+        usuario = findViewById(R.id.tvUsuario);
+        usuario.setText("Welcome "+ nombreUsuario + " " + emailUsuario);
     }
 }
--- app/src/main/java/com/example/proyectogit/MainActivity.java
+++ app/src/main/java/com/example/proyectogit/MainActivity.java
@@ -2,14 +2,40 @@ package com.example.proyectogit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button btnLogin;
+    EditText etNombre, etCorreo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //prueba git
+        btnLogin = findViewById(R.id.buttonLogin);
+        btnLogin.setOnClickListener(this);
+        etNombre = findViewById(R.id.editTextNombre);
+        etCorreo = findViewById(R.id.editTextCorreo);
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        String nombre = etNombre.getText().toString();
+        String correo = etCorreo.getText().toString();
+
+        if(!nombre.isEmpty() && !correo.isEmpty()){
+            Intent intentLogin = new Intent(this,HomeActivity.class);
+            intentLogin.putExtra("email",correo);
+            intentLogin.putExtra("nombre",nombre);
+            startActivity(intentLogin);
+
+        }
+
     }
 }
