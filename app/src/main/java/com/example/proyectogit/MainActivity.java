a3bba710beb0749cc35626790402d362f22992c1
EderInk <jossimar.leon.19@gmail.com>
2022-06-17 01:28:26 -0500

    Se ingresan validaciones en los EditText

--- app/src/main/java/com/example/proyectogit/MainActivity.java
+++ app/src/main/java/com/example/proyectogit/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.proyectogit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -35,7 +36,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             intentLogin.putExtra("email",correo);
             intentLogin.putExtra("nombre",nombre);
             startActivity(intentLogin);
+        }
+        /*if(nombre.isEmpty() && correo.isEmpty()){
+            etNombre.setError("Ingrese el nombre");
+            etCorreo.setError("Ingrese el correo");
+        }*/
 
+        if(!nombre.isEmpty() && correo.isEmpty()){
+            etCorreo.setError("Ingrese el correo electrónico");
+        }
+        if(nombre.isEmpty() && !correo.isEmpty()){
+            etNombre.setError("Ingrese el nombre");
+        }
+        else{
+            etNombre.setError("Ingrese el nombre");
+            etCorreo.setError("Ingrese el correo electrónico");
         }
 
     }
